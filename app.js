8cf74577be1c0436614b3361dc29a4711e6a891a
RobWat <find@A5-004015.localdomain>
2019-04-16 17:15:43 -0600

    final stuffs

---
+++ app.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+var no = document.getElementById('brand-logo').style.display = "none";
+
+console.log(yeet)
